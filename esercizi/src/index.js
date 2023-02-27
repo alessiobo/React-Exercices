c01aae3889bfdb6add0eff391c37fc5ad41b5e49
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-02-27 09:49:12 +0100

    feat:added components3

--- esercizi/src/index.js
+++ esercizi/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Hello} from "./Hello";
+import {App} from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello />
+    <App />
   </React.StrictMode>
 );
