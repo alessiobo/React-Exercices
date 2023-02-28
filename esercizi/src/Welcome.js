4930f67cd877300eb4600222365d12ef3ec89a87
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-02-28 13:03:43 +0100

    add:props4

--- esercizi/src/Welcome.js
+++ esercizi/src/Welcome.js
@@ -2,13 +2,11 @@ import React from "react";
 
 export class Welcome extends React.Component {
   render() {
-
     const name = this.props.name
-    const mic = name === undefined || name === '' ? 'New User' : name
 
     return (
       <div>
-        <p>Welcome, {mic}!</p>
+        <p>Welcome, {name}!</p>
         <p>Your age is: {this.props.age}</p>
       </div>
     );
