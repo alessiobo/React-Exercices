cec4e81af4a15bed32084c5a3329e83b7e6e432a
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-05 17:11:34 +0100

    add: conditionale-rendering1

--- esercizi/src/App.js
+++ esercizi/src/App.js
@@ -5,7 +5,7 @@ export function App(){
     return (
         <div>
             <Hello />
-            <Welcome name={<strong>Alessio</strong>} age={28} />
+            <Welcome name={<strong>Alessio</strong>} age={17} />
         </div>
     )
 }
--- esercizi/src/Welcome.js
+++ esercizi/src/Welcome.js
@@ -3,12 +3,12 @@ import { Age } from "./Age";
 
 export class Welcome extends React.Component {
   render() {
-    const name = this.props.name
+    const name = this.props.name;
 
     return (
       <div>
         <p>Welcome, {name}!</p>
-        <Age age={this.props.age}/>
+        {this.props.age > 18 && <Age age={this.props.age} />}
       </div>
     );
   }
