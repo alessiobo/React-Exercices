7a18e70b2e01ce9bfe8af7c5301dd57e37ccedf3
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-16 18:45:32 +0100

    add forms6

--- esercizi/src/UncontrolledLogin.js
+++ esercizi/src/UncontrolledLogin.js
@@ -1,15 +1,19 @@
-//Implement an UncontrolledLogin component that implements all the operations of the Login component,
-//but does so using uncontrolled components.
+//Modify the UncontrolledLogin component so that the username input is automatically focused when the 
+//component renders the first time.
 
 import React, { createRef } from "react";
 
 export class UncontrolledLogin extends React.Component {
   _formRef = createRef();
 
-  submitHandler = (event) => {
+  SubmitHandler = (event) => {
     event.preventDefault();
 
     const { username, password, check } = this._formRef.current.elements;
+
+    if (username.value === "" || password.value === "") {
+      return false;
+    }
 
     const data = {
       username: username.value,
@@ -20,7 +24,7 @@ export class UncontrolledLogin extends React.Component {
     this.props.onLogin(data);
   };
 
-  checkDisabledButton = () => {
+  checkButtonDisabled = () => {
     const { username, password, submit } = this._formRef.current.elements;
 
     if (username.value !== "" && password.value !== "") {
@@ -29,6 +33,9 @@ export class UncontrolledLogin extends React.Component {
       submit.disabled = true;
     }
   };
+  componentDidMount() {
+    this._formRef.current.elements.username.focus();
+  }
 
   render() {
     return (
