78e5aef471145b6313ddd91645ccb97c3ee86c08
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-21 09:47:17 +0100

    add useState2

--- esercizi/src/App.js
+++ esercizi/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
+import { Login } from "./Login";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ClickCounter />
+        <Login />
       </div>
     );
   }
