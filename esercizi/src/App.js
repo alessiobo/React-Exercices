f1674a7ed9d30fba515c8770efe45334ffbc3b5d
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-27 19:26:45 +0200

    add context1

--- esercizi/src/App.js
+++ esercizi/src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import TodoList from "./TodoList";
+import { LanguageContext } from "./LanguageContext";
+import DisplayLanguage from "./DisplayLanguage";
 
 class App extends React.Component {
+  state = {
+    language: "en",
+  };
+
+  handleChangeLang = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
   render() {
     return (
       <div>
-        <TodoList
-          render={(items, handlerRemove) => {
-            return <ul>
-              {items.map((item, index) => (
-                <li key={item + index}>
-                  {item}
-                  <button onClick={()=> handlerRemove(index)}>-</button>
-                </li>
-              ))}
-            </ul>
-          }}
-        />
+        <select onChange={this.handleChangeLang} value={this.state.language}>
+          <option value="en">English</option>
+          <option value="it">Italiano</option>
+        </select>
+        <LanguageContext.Provider value={this.state.language}>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
       </div>
     );
   }
---
+++ esercizi/src/DisplayLanguage.js
@@ -0,0 +1,29 @@
+// Create a LanguageContext and wrap a DisplayLanguage component within its provider.
+// Add a select tag to the component containing the DisplayLanguage component that allows
+// the user to select the language, and pass it as a value to the Provider.
+
+import React from "react";
+import { LanguageContext } from "./LanguageContext";
+
+const String = {
+  en: {
+    greetings: "Hello and welcome on this webpage",
+  },
+  it: {
+    greetings: "Ciao e benvenuto su questa pagina web",
+  },
+};
+
+class DisplayLanguage extends React.Component {
+  render() {
+    return (
+      <div>
+        <LanguageContext.Consumer>
+          {(language) => <div>{String[language].greetings}</div>}
+        </LanguageContext.Consumer>
+      </div>
+    );
+  }
+}
+
+export default DisplayLanguage;
