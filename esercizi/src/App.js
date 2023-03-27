71ae029bc805db21d08ef32c19752d0156b7a201
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-27 11:36:08 +0200

    add useRef

--- esercizi/src/App.js
+++ esercizi/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
-import { GitHubUser } from "./GitHubUser";
+import CarDetails from "./CarDetails";
+import React, { useState } from "react";
 
 function App() {
-  const username = "alessiobo";
+  const car1 = {
+    modello: "sportivo",
+    anno: 2000,
+    colore: "#9E0A0A",
+  };
+  const car2 = {
+    modello: "berlina",
+    anno: 2005,
+    colore: "#0A2BEF",
+  };
+  const car3 = {
+    modello: "coupè",
+    anno: 1950,
+    colore: "#006FE6",
+  };
+
   return (
     <div>
-      <GitHubUser username={username} />
+      <CarDetails initialData={car2} />
     </div>
   );
 }
