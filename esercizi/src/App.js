4d1c9a235102ffc23abe2c27585fc369fdabdf7c
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-27 19:23:30 +0200

    add render-props

--- esercizi/src/App.js
+++ esercizi/src/App.js
@@ -1,38 +1,25 @@
-import { Link, Route, Routes } from "react-router-dom";
-import Counter from "./Counter";
-import GithubUser from "./GithubUser";
-import GithubUserList from "./GithubUserList";
-import Welcome from "./Welcome";
+import React from "react";
+import TodoList from "./TodoList";
 
-function App() {
-  return (
-    <div>
-      <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/counter"}>Counter</Link>
-        <Link to={"/users"}>Users</Link>
-
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-
-          <Route path="/users" element={<GithubUserList />}>
-            <Route index element={<p>Add a user and select it</p>} />
-            <Route path=":username" element={<GithubUser />} />
-          </Route>
-
-          <Route path="/counter" element={<Counter />} />
-          <Route
-            path="*"
-            element={
-              <div>
-                <h1>404 Not Found</h1>
-              </div>
-            }
-          />
-        </Routes>
-      </nav>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <TodoList
+          render={(items, handlerRemove) => {
+            return <ul>
+              {items.map((item, index) => (
+                <li key={item + index}>
+                  {item}
+                  <button onClick={()=> handlerRemove(index)}>-</button>
+                </li>
+              ))}
+            </ul>
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
